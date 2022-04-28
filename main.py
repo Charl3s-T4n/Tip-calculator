f1a23e992632c71e9c5db8d9f89b18b9fd423a15
Charl3s-T4n <104561880+Charl3s-T4n@users.noreply.github.com>
2022-04-28 23:07:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #If the bill was $150.00, split between 5 people, with 12% tip. 
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Round the result to 2 decimal places.
+
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
@@ -10,12 +11,13 @@ tip_as_percent = tip / 100
 total_tip_amount = bill * tip_as_percent
 total_bill = bill + total_tip_amount
 bill_per_person = total_bill / people
-final_amount = round(bill_per_person, 2)
+final_amount = round(bill_per_person, 2)      # Round to 2 dec places 
 
 
-# FAQ: How to round to 2 decimal places?
-
-# Find the answer in the Q&A here: https://www.udemy.com/course/100-days-of-code/learn/lecture/17965132#questions/13315048
+# FAQ: How to round to 2 decimal places?  Use round(number, no. of dec places)
 
 
-print(f"Each person should pay: ${final_amount}")
+
+
+
+print(f"Each person should pay: ${final_amount}")    # Using f-string: useful as allows us to insert diff. data types into the string 
